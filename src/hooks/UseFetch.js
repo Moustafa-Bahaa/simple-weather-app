db25118c44d6c8950572e89a5ac52f8288e34fde
Moustafa Bahaa <moustafabahaa2060@gmail.com>
2023-08-24 05:38:18 +0300

    update

--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -2,7 +2,7 @@ const API_KEY = "18c0fcd463944b729a97e1d0cc91fa3d"
 
 export const useFetch = async (city, country) => {
     try {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}%2C${country}&appid=${API_KEY}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}%2C${country}&appid=${API_KEY}`)
         const weather = await response.json();
         return weather
     }
